3c2a6ebacf05eb93a7c25c5769ed12cb1f22120b
Akalanka-1996 <kasunakalanka4@gmail.com>
2021-06-26 03:33:49 +0530

    added

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CS_2017__001
+{
+    public partial class Form1 : Form
+       
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form2 frm2 = new Form2();
+            frm2.Show();
+            frm2.SetNumber(txtNumber.Text.ToString());
+        }
+
+        private void txtNumber_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
